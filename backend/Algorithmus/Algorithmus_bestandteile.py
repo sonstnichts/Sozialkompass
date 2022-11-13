542639d75eb9a041f68ae0fb51f70cbfad948787
Jonas Rettig <jrettig@uni-muenster.de>
2022-11-13 18:29:46 +0100

    further work on the implementation of spans, still not finished

--- backend/Algorithmus/Algorithmus_bestandteile.py
+++ backend/Algorithmus/Algorithmus_bestandteile.py
@@ -84,7 +84,24 @@ def spalten_loeschen(antragsliste_neu,frage):
     for delete_item in reversed(delete_columns):
         del antragsliste_neu[delete_item[0]]["Attribute"][delete_item[1]][frage]
 
-def append_new(antwortmoeglichkeiten, zeilen): #function that append new array values and correctly adapts correcting ones
+# TODO proper impelemntation of the last case
+# TODO check if the cases work as intended
+def append_new(antwortmoeglichkeiten, new): #function that append new array values and correctly adapts correcting the old arrays
+    for old in antwortmoeglichkeiten: #loops through the antwortmoeglichkeiten array
+        print(antwortmoeglichkeiten + "; " + new)
+        if old[0] == new[0]: #if the lower bound is the same
+            if old[1] == new[1]: #and if the higher bound is the same 
+                return antwortmoeglichkeiten #do nothing
+            else: #else
+                antwortmoeglichkeiten.insert(old+1, [new[1]+1, old[1]]) #insert a new array [higherBoundNew + 1, higherBoundOld] after the current array
+                old = new #set the added array as i
+        elif old[0] < new[0]: #if the new lower bound is higher than the old lower bound
+            (old-1)[1] = new[0]-1 #set the lower higherBound to the new lowerBound -1
+            antwortmoeglichkeiten.insert(old,new) #insert the new values at the current place
+            new[1] = old[0]-1 #set the new higher bound als the next lower bound - 1
+        elif old[0] > new[0]: #if the new lower bound is lower than the old lower bound
+            antwortmoeglichkeiten.insert(old, [new[1]+1, old[1]]) #insert a new array [higherBoundNew + 1, higherBoundOld] after the current array
+            old = new #set the added array as i
     return antwortmoeglichkeiten
 
 def antwortmöglichkeiten_generieren(antragsliste,frage,attribute):
@@ -100,7 +117,7 @@ def antwortmöglichkeiten_generieren(antragsliste,frage,attribute):
         # Im Fall Ganzzahl werden alle Möglichkeiten aus der Antragsliste mit allen Grenzen generiert
         # THIS CASE ISN'T FINAL
         # TODO check if the cast to string fucks something up elsewhere (it probably does)
-        # TODO implement append_new
+        # TODO finish implementation of append_new
         case "Ganzzahl":
             antwortmoeglichkeiten = [] #array of possible answer arrays
             antwortmoeglichkeitenFinal = [] #final array w/ string values
@@ -108,13 +125,16 @@ def antwortmöglichkeiten_generieren(antragsliste,frage,attribute):
             for antrag in antragsliste.items(): 
                 for bedingungen in antrag[1]["Attribute"]:
                     for zeilen in bedingungen.values(): 
-                        if len(antwortmoeglichkeiten) == 0: #if the antwortmöglichkeiten array is empty
+                        if not antwortmoeglichkeiten: #if the antwortmöglichkeiten array is empty
+                            print("bad, weil: " )
+                            print(antwortmoeglichkeiten)
                             if zeilen[0] != 0: #if the lower bound isnt zero
-                                antwortmoeglichkeiten.append([0, zeilen[1]-1]) #add a choice that is smaller than the array
+                                antwortmoeglichkeiten.append([0, zeilen[0]-1]) #add a choice that is smaller than the array
                             antwortmoeglichkeiten.append(zeilen) #add the actual array
                             antwortmoeglichkeiten.append([zeilen[1]+1, 1000000]) #add a choice that is bigger than the array
-                        else: append_new(antwortmoeglichkeiten, zeilen) #compilcated adding process
-            # print(antwortmoeglichkeiten)
+                        else:
+                            print("gut")
+                            append_new(antwortmoeglichkeiten, zeilen) #compilcated adding process
             for i in antwortmoeglichkeiten: 
                 antwortmoeglichkeitenFinal.append(str(i)) #casts all antwortmoeglichkeiten to a string for the tree generation
             return antwortmoeglichkeitenFinal #returns the string antwortmoeglichkeiten
