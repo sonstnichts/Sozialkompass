ffa5c15f6c50cddd8be10ee056be4f08af3270d7
Jonas Rettig <jrettig@uni-muenster.de>
2022-11-14 17:49:37 +0100

    further work on implementing spans, cases which call append_new are still miscalculated. also added the generated tree to .gitignore

--- backend/Algorithmus/Algorithmus_bestandteile.py
+++ backend/Algorithmus/Algorithmus_bestandteile.py
@@ -84,26 +84,6 @@ def spalten_loeschen(antragsliste_neu,frage):
     for delete_item in reversed(delete_columns):
         del antragsliste_neu[delete_item[0]]["Attribute"][delete_item[1]][frage]
 
-# TODO proper impelemntation of the last case
-# TODO check if the cases work as intended
-def append_new(antwortmoeglichkeiten, new): #function that append new array values and correctly adapts correcting the old arrays
-    for old in antwortmoeglichkeiten: #loops through the antwortmoeglichkeiten array
-        print(antwortmoeglichkeiten + "; " + new)
-        if old[0] == new[0]: #if the lower bound is the same
-            if old[1] == new[1]: #and if the higher bound is the same 
-                return antwortmoeglichkeiten #do nothing
-            else: #else
-                antwortmoeglichkeiten.insert(old+1, [new[1]+1, old[1]]) #insert a new array [higherBoundNew + 1, higherBoundOld] after the current array
-                old = new #set the added array as i
-        elif old[0] < new[0]: #if the new lower bound is higher than the old lower bound
-            (old-1)[1] = new[0]-1 #set the lower higherBound to the new lowerBound -1
-            antwortmoeglichkeiten.insert(old,new) #insert the new values at the current place
-            new[1] = old[0]-1 #set the new higher bound als the next lower bound - 1
-        elif old[0] > new[0]: #if the new lower bound is lower than the old lower bound
-            antwortmoeglichkeiten.insert(old, [new[1]+1, old[1]]) #insert a new array [higherBoundNew + 1, higherBoundOld] after the current array
-            old = new #set the added array as i
-    return antwortmoeglichkeiten
-
 def antwortmöglichkeiten_generieren(antragsliste,frage,attribute):
 
     attributkategorie = attribute[frage]["Kategorie"]
@@ -112,29 +92,57 @@ def antwortmöglichkeiten_generieren(antragsliste,frage,attribute):
 
         # Im Fall Auswahl werden die Antwortmöglichkeiten aus der Attributenliste zurückgegeben
         case "Auswahl":
+            print(attribute[frage]["Antwortmoeglichkeiten"])
             return attribute[frage]["Antwortmoeglichkeiten"]
         
         # Im Fall Ganzzahl werden alle Möglichkeiten aus der Antragsliste mit allen Grenzen generiert
-        # THIS CASE ISN'T FINAL
-        # TODO check if the cast to string fucks something up elsewhere (it probably does)
-        # TODO finish implementation of append_new
+        # * I think this case should work now, someone else should have a look over this though!
+        # TODO check if the cast to string works with the tree generation
         case "Ganzzahl":
             antwortmoeglichkeiten = [] #array of possible answer arrays
             antwortmoeglichkeitenFinal = [] #final array w/ string values
 
-            for antrag in antragsliste.items(): 
+            for antrag in antragsliste.items():
                 for bedingungen in antrag[1]["Attribute"]:
                     for zeilen in bedingungen.values(): 
-                        if not antwortmoeglichkeiten: #if the antwortmöglichkeiten array is empty
-                            print("bad, weil: " )
-                            print(antwortmoeglichkeiten)
-                            if zeilen[0] != 0: #if the lower bound isnt zero
-                                antwortmoeglichkeiten.append([0, zeilen[0]-1]) #add a choice that is smaller than the array
-                            antwortmoeglichkeiten.append(zeilen) #add the actual array
-                            antwortmoeglichkeiten.append([zeilen[1]+1, 1000000]) #add a choice that is bigger than the array
-                        else:
-                            print("gut")
-                            append_new(antwortmoeglichkeiten, zeilen) #compilcated adding process
+                        if str(bedingungen.keys()) == ("dict_keys(['" + str(frage) + "'])"): #ensures that only the relevant int attributes are used, this solution is kinda bad though
+                            if not antwortmoeglichkeiten: #if the antwortmöglichkeiten array is empty
+                                if zeilen[0] != 0: #if the lower bound isnt zero
+                                    antwortmoeglichkeiten.append([0, zeilen[0]-1]) #add a choice that is smaller than the array
+                                antwortmoeglichkeiten.append(zeilen) #add the actual array
+                                antwortmoeglichkeiten.append([zeilen[1]+1, 1000000]) #add a choice that is bigger than the array
+                            else:
+                                append_new(antwortmoeglichkeiten, zeilen) #compilcated adding process
             for i in antwortmoeglichkeiten: 
                 antwortmoeglichkeitenFinal.append(str(i)) #casts all antwortmoeglichkeiten to a string for the tree generation
             return antwortmoeglichkeitenFinal #returns the string antwortmoeglichkeiten
+
+# ! Some maths are wrong here. There are cases where a value can sink below 0, and the last upper bound doesn't seem to work
+# ! Also the cases are apparantly sometimes miscategorized
+# TODO proper implementation of the last case
+# TODO fix maths
+# TODO check if the cases work as intended
+def append_new(answers, new): #function that append new array values and correctly adapts correcting the old arrays
+    for old in answers: #loops through the antwortmoeglichkeiten array
+        if old[0] == new[0]: #if the lower bound is the same
+            if old[1] == new[1]: #and if the higher bound is the same 
+                break #do nothing
+            else: #else
+                index = answers.index(old) #gets the position of the current object
+                answers.insert(index+1, [new[1]+1, old[1]]) #insert a new array [higherBoundNew + 1, higherBoundOld] after the current array
+                old = new #set the added array as i
+                break
+        elif old[0] < new[0]: #if the new lower bound is higher than the old lower bound
+            index = answers.index(old) #gets the position of the current object
+            answers[index-1][1] = new[0]-1 #set the lower higherBound to the new lowerBound -1
+            answers.insert(index,new) #insert the new values at the current place
+            new[1] = old[0]-1 #!bad math, can be -1; set the new higher bound als the next lower bound - 1 
+            break
+        elif old[0] > new[0]: #if the new lower bound is lower than the old lower bound
+            index = answers.index(old) #gets the position of the current object
+            answers.insert(index, [new[1]+1, old[1]]) #insert a new array [higherBoundNew + 1, higherBoundOld] after the current array
+            old = new #set the added array as i
+            break
+        else:
+            break
+    return answers
